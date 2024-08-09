34b8cb63d36cc5d3baaffe841a45cc91f4c53253
HilFing <81067543+hilfing@users.noreply.github.com>
2024-08-09 22:00:40 +0530

    Delete app directory

--- app/src/main/java/org/paulstudios/datasurvey/data/models/Project.kt
+++
@@ -1,7 +0,0 @@
-package org.paulstudios.datasurvey.data.models
-
-data class Project(
-    val project_id: String,
-    val project_name: String,
-    val project_description: String
-)
--- app/src/main/java/org/paulstudios/datasurvey/data/models/Screen.kt
+++
@@ -1,8 +0,0 @@
-package org.paulstudios.datasurvey.data.models
-
-sealed class Screen(val route: String) {
-    object Login : Screen("login")
-    object Register : Screen("register")
-    object DataCollection : Screen("data_collection")
-    object ProjectIdForm : Screen("project_id_form")
-}
